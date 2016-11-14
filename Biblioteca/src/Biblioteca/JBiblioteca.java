e1a9c8d6deb5f2abf2ee7a2242e511b93cad9662
AnDeR <AnDeR@192.168.0.14>
2016-11-14 20:50:04 +0100

    class libro

---
+++ Biblioteca/src/Biblioteca/JBiblioteca.java
@@ -0,0 +1,101 @@
+package Biblioteca;
+
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class JBiblioteca extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		//test
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JBiblioteca frame = new JBiblioteca();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JBiblioteca() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 642, 416);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(64, 34, 116, 22);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(462, 34, 116, 22);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblIsbn = new JLabel("ISBN:");
+		lblIsbn.setBounds(12, 37, 56, 16);
+		contentPane.add(lblIsbn);
+		
+		JLabel lblAutor = new JLabel("Autor:");
+		lblAutor.setBounds(394, 37, 56, 16);
+		contentPane.add(lblAutor);
+		
+		JButton btnConsultar = new JButton("CONSULTAR");
+		btnConsultar.setBounds(243, 86, 116, 25);
+		contentPane.add(btnConsultar);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 124, 600, 167);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"AUTOR", "ISBN", "TITULO"
+			}
+		));
+		scrollPane.setViewportView(table);
+		
+		JButton btnNuevo = new JButton("NUEVO");
+		btnNuevo.setBounds(64, 304, 97, 25);
+		contentPane.add(btnNuevo);
+		
+		JButton btnEliminar = new JButton("ELIMINAR");
+		btnEliminar.setBounds(262, 304, 97, 25);
+		contentPane.add(btnEliminar);
+		
+		JButton btnModificar = new JButton("MODIFICAR");
+		btnModificar.setBounds(430, 304, 109, 25);
+		contentPane.add(btnModificar);
+	}
+}
---
+++ Biblioteca/src/Biblioteca/Libro.java
@@ -0,0 +1,71 @@
+package Biblioteca;
+
+import java.io.Serializable;
+
+import Arbol.Arbol;
+
+public class Libro implements Serializable{
+	private String autor;
+	private String titulo;
+	private String iSBN;
+	
+	public Libro(String iSBN){
+		this.iSBN=iSBN;
+		
+	}
+	public Libro(String autor,String titulo, String iSBN){
+		this.autor=autor;
+		this.titulo=titulo;
+		this.iSBN=iSBN;
+		
+	}
+	@Override
+	public String toString() {
+		return autor.toLowerCase()+","+titulo.toLowerCase()+","+iSBN.toLowerCase();
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getiSBN() {
+		return iSBN;
+	}
+	public void setiSBN(String iSBN) {
+		this.iSBN = iSBN;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		
+		result = prime * result + ((iSBN == null) ? 0 : iSBN.hashCode());
+		
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		if (iSBN == null) {
+			if (other.iSBN != null)
+				return false;
+		} else if (!iSBN.equals(other.iSBN))
+			return false;
+		return true;
+	}
+	
+
+}
